dd6553aa793cd7055848d9fc70fedd8a9fbc8845
Ruben Martin <rubenmromero@gmail.com>
2021-02-08 07:52:49 +0100

    Improve SSH tunnel script

--- project_env_service-local_port.sh
+++ project_env_service-local_port.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-JUMP_HOST="<user>@<hop_host>"
-PORT_FORWARDING="<local_port>:<target_host>:<target_port>"
+HOP_HOST="[-i <priv_key_file_path> ]<user>@<hop_host>"
+TARGET_HOST=<target_host>
+TARGET_PORT=<target_port>
 
+PORT_FORWARDING="1$TARGET_PORT:$TARGET_HOST:$TARGET_PORT"
 PID=$(/bin/ps -ef |grep "$PORT_FORWARDING" |grep -v grep |awk '{print $2}')
 if [[ -z $PID ]]
 then
-    /usr/bin/ssh -fN -g -L $PORT_FORWARDING $JUMP_HOST
+    /usr/bin/ssh -fN -g -L $PORT_FORWARDING $HOP_HOST
 else
-    echo -e "\nThe SSH tunnel is already running with process ID => ${PID}\n" 
+    echo -e "\nThe SSH tunnel is already running with process ID => ${PID}\n"
     exit 1
 fi
 
 PID=$(/bin/ps -ef |grep "$PORT_FORWARDING" |grep -v grep |awk '{print $2}')
-echo -e "\nFanfare! SSH tunnel up & running!"
+echo -e "\nFanfare! SSH tunnel up & running in the local port 1$TARGET_PORT!"
 echo -e "\nProcess ID of SSH tunnel => ${PID}\n"
